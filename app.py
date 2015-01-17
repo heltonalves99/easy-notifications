5cda68576c47ea489af5eabbf9a2257c385dc3b5
Rael Max <contato@raelmax.com>
2015-01-17 17:12:26 -0300

    basic api to add devices

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from bottle import route, run, template
+from bottle import Bottle, run, request
+from bottle.ext.mongo import MongoPlugin
+
+app = Bottle()
+
+mongo = MongoPlugin(uri="mongodb://127.0.0.1",
+                    db="easy_notifications",
+                    json_mongo=True,
+                    keyword='db')
+
+app.install(mongo)
 
 
-@route('/')
-def index():
-    return template('base')
+@app.post('/api/device')
+def add_device(db):
+    token = request.forms.get('token', None)
+    if token:
+        id = db['devices'].insert({'device': token})
+        return {'id': str(id)}
+    return {'error': '"token" is required.'}
 
-run(host='localhost', port='8080', debug=True)
+
+run(app, host='localhost', port='8080', debug=True, reloader=True)
