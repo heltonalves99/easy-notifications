2dec279be62493f092525b4a62d4e6aed9ba7f7b
Rael Max <contato@raelmax.com>
2015-01-19 14:58:43 -0300

    catch client errors using enhanced mode

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import random
 from apns import APNs, Payload
 from bottle import Bottle, run, request
 from bottle.ext.mongo import MongoPlugin
@@ -31,14 +32,21 @@ def send_push(db):
     if device and custom_payload:
         device = db['devices'].find_one({'_id': ObjectId(device)})
 
-        apns = APNs(use_sandbox=False,
-                    cert_file='aps_production.pem',
-                    key_file='aps_production_key.pem')
+        apns = APNs(use_sandbox=True,
+                    cert_file='aps_voip.pem',
+                    key_file='aps_voip_key.pem', enhanced=True)
 
         # Send a notification
         custom_payload = json.loads(custom_payload)
         payload = Payload(**custom_payload)
-        apns.gateway_server.send_notification(device['device'], payload)
+        identifier = random.getrandbits(32)
+
+        apns.gateway_server.send_notification(device['device'], payload, identifier=identifier)
+
+        def response_listener(error_response):
+            print "client get error-response: " + str(error_response)
+
+        apns.gateway_server.register_response_listener(response_listener)
     else:
         return {'error': '"device" and "payload" are required.'}
 
