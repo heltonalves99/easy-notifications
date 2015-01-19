0fb8705861a7fb71c4dbdc3a00f5f37979ed846b
Rael Max <contato@raelmax.com>
2015-01-19 13:23:13 -0300

    added basic push message endpoint

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import json
+from apns import APNs, Payload
 from bottle import Bottle, run, request
 from bottle.ext.mongo import MongoPlugin
+from bson.objectid import ObjectId
 
 app = Bottle()
 
@@ -19,5 +22,26 @@ def add_device(db):
         return {'id': str(id)}
     return {'error': '"token" is required.'}
 
+
+@app.post('/api/push')
+def send_push(db):
+    device = request.forms.get('device', None)
+    custom_payload = request.forms.get('payload', None)
+
+    if device and custom_payload:
+        device = db['devices'].find_one({'_id': ObjectId(device)})
+
+        apns = APNs(use_sandbox=False,
+                    cert_file='aps_production.pem',
+                    key_file='aps_production_key.pem')
+
+        # Send a notification
+        custom_payload = json.loads(custom_payload)
+        payload = Payload(**custom_payload)
+        apns.gateway_server.send_notification(device['device'], payload)
+    else:
+        return {'error': '"device" and "payload" are required.'}
+
+    return {'device': device, 'payload': custom_payload}
 
 run(app, host='localhost', port='8080', debug=True, reloader=True)
