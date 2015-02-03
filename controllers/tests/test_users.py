8bf8d59dab1c2692740970334cc9d0378110617a
Rael Max <contato@raelmax.com>
2015-02-02 21:39:24 -0300

    fix users test

--- controllers/tests/test_users.py
+++ controllers/tests/test_users.py
@@ -1,34 +1,20 @@
-import unittest
-
-from passlib.hash import sha256_crypt
-from models.certificates import Certificate
-from models.devices import Device
-from models.users import User
 from . import BaseTest
 
 
 class TestUser(BaseTest):
     def setUp(self):
         super(TestUser, self).setUp()
-        self.base_url = '/api/devices'
-        self.user1 = User(
-            username="john",
-            email="johmail@foobar.com",
-            password=sha256_crypt.encrypt("password")
-        )
+        self.base_url = '/api/users'
 
-        self.db.add(self.user1)
-        self.db.commit()
+    def test_create_and_authenticate_user(self):
+        user = {
+            'username': 'john',
+            'email': 'johmail@foobar.com',
+            'password': 'password'
+        }
+        response = self.test_app.post(self.base_url, user, expect_errors=True)
+        self.assertEqual(response.status_int, 200)
 
-    def _auth(self):
         self.test_app.authorization = ('Basic', ('john', 'password'))
-
-    def test_access_denied(self):
-        self.test_app.authorization = ('Basic', ('john', 'password-wrong'))
-        response = self.test_app.get(self.base_url, expect_errors=True)
-        self.assertEqual(response.status_int, 401)
-
-    def test_access_granted(self):
-        self._auth()
-        response = self.test_app.get(self.base_url, expect_errors=True)
+        response = self.test_app.get('/api/certificates', expect_errors=True)
         self.assertEqual(response.status_int, 200)
