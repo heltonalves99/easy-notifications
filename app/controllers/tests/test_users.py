7a6f4bc907deed3b838dfe3aa2e0f9eb2202bd46
Helton Alves <helton99@hotmail.com>
2015-02-10 12:19:47 -0300

    Added the test method if already exist a user registred.

--- app/controllers/tests/test_users.py
+++ app/controllers/tests/test_users.py
@@ -6,15 +6,31 @@ class TestUser(BaseTest):
         super(TestUser, self).setUp()
         self.base_url = '/api/users'
 
-    def test_create_and_authenticate_user(self):
-        user = {
+        self.user = {
             'username': 'john',
             'email': 'johmail@foobar.com',
             'password': 'password'
         }
-        response = self.test_app.post(self.base_url, user, expect_errors=True)
+
+    def test_create_and_authenticate_user(self):
+        
+        response = self.test_app.post(self.base_url, self.user, expect_errors=True)
         self.assertEqual(response.status_int, 200)
 
         self.test_app.authorization = ('Basic', ('john', 'password'))
         response = self.test_app.get('/api/certificates', expect_errors=True)
         self.assertEqual(response.status_int, 200)
+
+    def test_verify_exist_user_registered(self):
+        self.test_app.post(self.base_url, self.user, expect_errors=True)
+
+        self.user['username'] = 'coisa'
+
+        response = self.test_app.post(self.base_url, self.user, expect_errors=True)
+        self.assertEqual(response.status_int, 406)
+
+        self.user['username'] = 'john'
+        self.user['email'] = 'coisa@coisa.com'
+
+        response = self.test_app.post(self.base_url, self.user, expect_errors=True)
+        self.assertEqual(response.status_int, 406)
