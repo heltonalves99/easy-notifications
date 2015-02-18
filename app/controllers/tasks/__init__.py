80282b5e264d0b6f42104edbe51bb5a070652dd0
Rael Max <contato@raelmax.com>
2015-02-18 18:37:53 -0300

    remove python-rq and start connections(pubsub) as threads

--- app/controllers/tasks/__init__.py
+++ app/controllers/tasks/__init__.py
@@ -1,48 +1,55 @@
 import json
+import threading
 
 from redis import StrictRedis
 from apnsclient import Message, APNs, Session
 
-redis_db = StrictRedis()
 
+class PushListener(threading.Thread):
+    def __init__(self, certificate):
+        threading.Thread.__init__(self)
 
-def send_notifications(certificate):
-    ps = redis_db.pubsub()
-    ps.subscribe(certificate.token)
-    apns_session = Session()
+        self.redis = StrictRedis()
+        self.pubsub = self.redis.pubsub()
+        self.pubsub.subscribe(certificate.token)
 
-    for push in ps.listen():
-        if push['type'] == 'message':
-            con = apns_session.get_connection(certificate.cert_type,
-                                              cert_string=certificate.cert_pem,
-                                              key_string=certificate.key_pem)
+        self.apns_session = Session()
+        self.con = self.apns_session.get_connection(certificate.cert_type,
+                                                    cert_string=certificate.cert_pem,
+                                                    key_string=certificate.key_pem)
 
-            push_data = json.loads(push['data'])
-            devices = push_data['devices']
-            payload = push_data['payload']
+    def _send_notification(self, message):
+        push_data = json.loads(message)
+        devices = push_data['devices']
+        payload = push_data['payload']
 
-            message = Message(devices, **payload)
+        _message = Message(devices, **payload)
 
-            # Send the message.
-            srv = APNs(con)
-            try:
-                res = srv.send(message)
-            except Exception as e:
-                print e
-            else:
-                # Check failures. Check codes in APNs reference docs.
-                for token, reason in res.failed.items():
-                    code, errmsg = reason
-                    # according to APNs protocol the token reported here
-                    # is garbage (invalid or empty), stop using and remove it.
-                    print "Device failed: {0}, reason: {1}".format(token, errmsg)
+        # Send the message.
+        srv = APNs(self.con)
+        try:
+            res = srv.send(_message)
+        except Exception as e:
+            print e
+        else:
+            # Check failures. Check codes in APNs reference docs.
+            for token, reason in res.failed.items():
+                code, errmsg = reason
+                # according to APNs protocol the token reported here
+                # is garbage (invalid or empty), stop using and remove it.
+                print "Device failed: {0}, reason: {1}".format(token, errmsg)
 
-                # Check failures not related to devices.
-                for code, errmsg in res.errors:
-                    print "Error: {}".format(errmsg)
+            # Check failures not related to devices.
+            for code, errmsg in res.errors:
+                print "Error: {}".format(errmsg)
 
-                # Check if there are tokens that can be retried
-                if res.needs_retry():
-                    pass
-                    # repeat with retry_message or reschedule your task
-                    # retry_message = res.retry()
+            # Check if there are tokens that can be retried
+            if res.needs_retry():
+                pass
+                # repeat with retry_message or reschedule your task
+                # retry_message = res.retry()
+
+    def run(self):
+        for item in self.pubsub.listen():
+            if item['type'] == "message":
+                self._send_notification(item['data'])
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from redis import StrictRedis
-from rq import Queue, cancel_job
 from bottle import run
 from app import main
-from app.controllers.tasks import send_notifications
+from app.controllers.tasks import PushListener
 from app.models import session
 from app.models.certificates import Certificate
 
@@ -10,18 +9,14 @@ db = session()
 redis_db = StrictRedis()
 
 
-def reconnect_apns():
+def connect_apns():
     certificates = db.query(Certificate).all()
-    queue = Queue('pushs', connection=redis_db)
 
-    # clearing connections
-    for job in queue.job_ids:
-        cancel_job(job, connection=redis_db)
-
-    # reconnecting
+    # start certificates threads
     for cert in certificates:
-        queue.enqueue(send_notifications, cert)
+        listener = PushListener(cert)
+        listener.start()
 
 if __name__ == '__main__':
-    reconnect_apns()
-    run(main, host='localhost', port='8080', debug=True, reloader=True)
+    connect_apns()
+    run(main, host='localhost', port='8080', debug=True)
